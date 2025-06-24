4dcb1dd26b0a78d403d351774b3be5d0ee340b40
mateja503 <matejanikolikj503@gmail.com>
2025-06-25 00:09:09 +0200

    added the tittle property for the TravelPackage entity

--- TravelAgency.Domain/Models/TravelPackage.cs
+++ TravelAgency.Domain/Models/TravelPackage.cs
@@ -13,6 +13,7 @@ namespace TravelAgency.Domain.Models
         [Key]
         public int Id { get; set; }
 
+        public string Tittle { get; set; }
         public string Description { get; set; }
 
         public int Capacity { get; set; }
---
+++ TravelAgency.Repository/Data/Migrations/20250624214951_AddTittleForTheTravelPackage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAgency.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTittleForTheTravelPackage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tittle",
+                table: "TravelPackages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tittle",
+                table: "TravelPackages");
+        }
+    }
+}
