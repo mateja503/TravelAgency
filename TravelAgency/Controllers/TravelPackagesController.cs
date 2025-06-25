8c49487a1efd8d4d1e0a50e7871316153e028e49
mateja503 <matejanikolikj503@gmail.com>
2025-06-25 16:57:10 +0200

    Added the create option for TravelPackage

---
+++ TravelAgency/Controllers/TravelPackagesController.cs
@@ -0,0 +1,162 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency.Domain.Models;
+using TravelAgency.Domain.ValueObjects;
+using TravelAgency.Service.Interface;
+using TravelAgency.ViewModels;
+
+namespace TravelAgency.Controllers
+{
+    public class TravelPackagesController(ITravelPackageService travelpackageService): Controller
+    {
+        private readonly ITravelPackageService _travelpackageService = travelpackageService;
+       
+        // GET: TravelPackages
+        public async Task<IActionResult> Index()
+        {
+            return View(await _travelpackageService.GetAll());
+        }
+
+        // GET: TravelPackages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travelPackage = await _travelpackageService.GetById(id ?? 0);
+            if (travelPackage == null)
+            {
+                return NotFound();
+            }
+
+            return View(travelPackage);
+        }
+
+        // GET: TravelPackages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TravelPackages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TravelPackageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var travelPackage = new TravelPackage
+                {
+                    Tittle = model.Tittle,
+                    Description = model.Description,
+                    Capacity = model.Capacity,
+                    Price = new Price 
+                    {
+                        Amount = model.Amount,
+                        TypeCurrencty = model.TypeCurrency
+                    },
+                    DateRange = new DateRange 
+                    {
+                        From = model.From,
+                        To = model.To,
+                    }
+                    
+                };
+                await _travelpackageService.Add(travelPackage);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: TravelPackages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travelPackage = await _travelpackageService.GetById(id ?? 0);
+            if (travelPackage == null)
+            {
+                return NotFound();
+            }
+            return View(travelPackage);
+        }
+
+        // POST: TravelPackages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Tittle,Description,Capacity")] TravelPackage travelPackage)
+        {
+            if (id != travelPackage.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _travelpackageService.Update(id, travelPackage);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TravelPackageExists(travelPackage.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(travelPackage);
+        }
+
+        // GET: TravelPackages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travelPackage = await _travelpackageService.GetById(id ?? 0);
+            if (travelPackage == null)
+            {
+                return NotFound();
+            }
+
+            return View(travelPackage);
+        }
+
+        // POST: TravelPackages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var travelPackage = await  _travelpackageService.GetById(id);
+            if (travelPackage != null)
+            {
+                await _travelpackageService.DeleteById(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private  bool TravelPackageExists(int id)
+        {
+            return _travelpackageService.GetAll().GetAwaiter().GetResult().Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TravelAgency/ViewModels/TravelPackageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using TravelAgency.Domain.Shared;
+
+namespace TravelAgency.ViewModels
+{
+    public record TravelPackageViewModel
+    {
+        public string Tittle { get; init; }
+
+        public string Description { get; init; }
+
+        public int Capacity { get; init; }
+
+        public double Amount { get; init; }
+
+        public TypeCurrency TypeCurrency { get; init; }
+
+        public DateTime From { get; init; }
+
+        public DateTime To { get; init; }
+
+    }
+}
